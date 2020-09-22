a1277b59458e6330691b411a395c148585fbeddb
skit <1441519420@qq.com>
2020-09-22 23:28:05 +0800

    创建项目

---
+++ github_test/src/test/java/com/skit/github_test/GithubTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.skit.github_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        // 这是我的测试代码1
+    }
+
+}
